60a99f470342a27a809b52c838f8da9c4af09673
Nicholas Restifo <nsrestifo@hotmail.com>
2017-06-20 21:58:03 -0400

    default repository stub

---
+++ src/main/java/repository/DefaultRepository.java
@@ -0,0 +1,37 @@
+package repository;
+
+import io.katharsis.queryspec.QuerySpec;
+import io.katharsis.repository.ResourceRepositoryV2;
+import io.katharsis.resource.list.ResourceList;
+
+import java.io.Serializable;
+
+public class DefaultRepository implements ResourceRepositoryV2 {
+    public Class getResourceClass() {
+        return null;
+    }
+
+    public Object findOne(Serializable serializable, QuerySpec querySpec) {
+        return null;
+    }
+
+    public ResourceList findAll(QuerySpec querySpec) {
+        return null;
+    }
+
+    public ResourceList findAll(Iterable iterable, QuerySpec querySpec) {
+        return null;
+    }
+
+    public void delete(Serializable serializable) {
+
+    }
+
+    public Object create(Object o) {
+        return null;
+    }
+
+    public Object save(Object o) {
+        return null;
+    }
+}
