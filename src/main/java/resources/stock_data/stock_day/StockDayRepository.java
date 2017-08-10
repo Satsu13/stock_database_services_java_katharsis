474606a1e7b0de47e17e3101beddbf4aadee3893
Nicholas Restifo <nsrestifo@hotmail.com>
2017-08-09 21:44:46 -0400

    added repository stubs

---
+++ src/main/java/resources/stock_data/stock_day/StockDayRepository.java
@@ -0,0 +1,37 @@
+package resources.stock_data.stock_day;
+
+import io.katharsis.queryspec.QuerySpec;
+import io.katharsis.repository.ResourceRepositoryV2;
+import io.katharsis.resource.list.ResourceList;
+
+import java.io.Serializable;
+
+public class StockDayRepository implements ResourceRepositoryV2 {
+    public Class getResourceClass() {
+        return null;
+    }
+
+    public Object findOne(Serializable serializable, QuerySpec querySpec) {
+        return null;
+    }
+
+    public ResourceList findAll(QuerySpec querySpec) {
+        return null;
+    }
+
+    public ResourceList findAll(Iterable iterable, QuerySpec querySpec) {
+        return null;
+    }
+
+    public void delete(Serializable serializable) {
+
+    }
+
+    public Object create(Object o) {
+        return null;
+    }
+
+    public Object save(Object o) {
+        return null;
+    }
+}
