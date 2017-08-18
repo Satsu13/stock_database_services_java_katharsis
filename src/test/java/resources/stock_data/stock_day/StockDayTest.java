bc6acffaa7335975ae5e2cfc05823b2273e4ccc7
Nicholas Restifo <nsrestifo@hotmail.com>
2017-08-17 23:04:42 -0400

    initial test

---
+++ src/test/java/resources/stock_data/stock_day/StockDayTest.java
@@ -0,0 +1,22 @@
+package resources.stock_data.stock_day;
+
+import io.katharsis.client.KatharsisClient;
+import io.katharsis.queryspec.QuerySpec;
+import io.katharsis.repository.ResourceRepositoryV2;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNotEquals;
+
+public class StockDayTest {
+
+    @Test
+    public void testName() throws Exception {
+        KatharsisClient client = new KatharsisClient("http://192.168.99.100:3000/stock-histories/1");
+        ResourceRepositoryV2<StockDay, Long> repo = client.getRepositoryForType(StockDay.class);
+        QuerySpec querySpec = new QuerySpec(StockDay.class);
+        List<StockDay> days = repo.findAll(querySpec);
+        assertNotEquals(0, days.size());
+    }
+}
