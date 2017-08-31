cff53f919b31accdaa78e922001efef5b7d80d36
Nicholas Restifo <nsrestifo@hotmail.com>
2017-08-30 22:04:54 -0400

    fixed query spec class (doesn't seem to matter?)

--- src/test/java/resources/stock_data/stock_history/StockHistoryTest.java
+++ src/test/java/resources/stock_data/stock_history/StockHistoryTest.java
@@ -4,7 +4,6 @@ import io.katharsis.client.KatharsisClient;
 import io.katharsis.queryspec.QuerySpec;
 import io.katharsis.repository.ResourceRepositoryV2;
 import org.junit.Test;
-import resources.stock_data.stock_day.StockDay;
 
 import java.util.List;
 
@@ -15,7 +14,7 @@ public class StockHistoryTest {
     public void testName() throws Exception {
         KatharsisClient client = new KatharsisClient("http://192.168.99.100:3000/");
         ResourceRepositoryV2<StockHistory, Long> repo = client.getRepositoryForType(StockHistory.class);
-        QuerySpec querySpec = new QuerySpec(StockDay.class);
+        QuerySpec querySpec = new QuerySpec(StockHistory.class);
         List<StockHistory> histories = repo.findAll(querySpec);
         assertNotEquals(0, histories.size());
     }
